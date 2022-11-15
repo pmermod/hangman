d29ed5b0b4d9eed40d4007be0d65cfa522ac3fc4
pmermod <93401090+pmermod@users.noreply.github.com>
2022-11-15 13:07:33 -0500

    Remove sample code from class lecture

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
 import prompt from "readline-sync";
 import wordBank from "./word-bank.js";
-
-console.log(wordBank);
-
-let feedback = prompt.question("How's it going?");
-console.log(feedback);
-
-if (feedback === "Good") {
-  console.log("I'm glad!");
-} else {
-  console.log("I'm so sorry to hear that.");
-}

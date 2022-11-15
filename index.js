00075d073a25ccb9740c83ebe997b83cffd791f9
pmermod <93401090+pmermod@users.noreply.github.com>
2022-11-15 14:43:02 -0500

    Initial console logs to user

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import prompt from "readline-sync";
 import wordBank from "./word-bank.js";
+
+console.log(
+  "Lookout! \n .... It's HaNGMaN!\n(the gallows are creeky)\n\nPress Control + C to exit."
+);

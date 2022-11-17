d99f0ac903c287c966a6ec588c3b2007e45d641b
pmermod <93401090+pmermod@users.noreply.github.com>
2022-11-16 23:52:05 -0500

    Code beginning of game

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ import prompt from "readline-sync";
 import wordBank from "./word-bank.js";
 
 console.log(
-  "Lookout! \n .... It's HaNGMaN!\n(the gallows are creeky)\n\nPress Control + C to exit."
+  "Lookout! \n .... It's HaNGMaN!\n(the gallows are creeky)\n\nPress ctrl + c to exit."
 );
+
+console.log(
+  "Guess the letters that make up the secret word. You are allowed six wrong guesses before you are hung."
+);
+
+let randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+let wordSplit = randomWord.split("");
+let blanksLength = wordSplit.length;
+blanksLength;
+
+// ******** Figure out how to create an array of blanks whose length equals length of wordSplit

c448b178dfa1b3abb47de4a2ba53371ef1918e6d
pmermod <93401090+pmermod@users.noreply.github.com>
2022-11-19 16:49:16 -0500

    continue initial work - before loops

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import prompt from "readline-sync";
-import wordBank from "./word-bank.js";
+//import wordBank from "./word-bank.js";
+const wordBank = ["win"];
 
 console.log(
-  "Lookout! \n .... It's HaNGMaN!\n(the gallows are creeky)\n\nPress ctrl + c to exit."
+  "\nLookout! \n .... It's HaNGMaN!\n\nPress ctrl + c to exit.\n\n\n"
 );
 
 console.log(
-  "Guess the letters that make up the secret word. You are allowed six wrong guesses before you are hung."
+  "Guess the letters that make up the secret word, one letter at a time. \nYou are allowed six wrong guesses before you are hung.\n"
 );
 
 let randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-let wordArray = randomWord.split("");
+let wordLowCase = randomWord.toLowerCase();
+let wordArray = wordLowCase.split("");
 let blanksLength = wordArray.length;
 
 // test
@@ -18,18 +20,49 @@ wordArray;
 
 let blankString = "_"; // ^remove line above, just a test
 
-// this below doesn't work, only prints one "_" in Quokka
-/*
-
-const printBlanks = (blanksLength) => {
-  console.log(blankString + " ".repeat(blanksLength));
-};
-printBlanks(blanksLength)
-
-*/
-
 let blanksArray = blankString.repeat(blanksLength).split("");
-console.log(blanksArray);
 
 // test
-console.log(blanksArray.length);
+blanksArray;
+
+// test
+//console.log(blanksArray.length);
+
+let allBlanksString = blanksArray.join(" ");
+//console.log(allBlanksString);
+console.log(allBlanksString);
+console.log("^");
+
+let letter = prompt.question("Enter a letter: ");
+console.log(letter);
+
+//let letter = "e"; //only to test --  remove!
+
+let letterLower = letter.toLowerCase();
+let containsLetter = wordArray.includes(letterLower);
+
+/*
+if (containsLetter) {
+  let letterIndex = wordArray.indexOf(letterLower);
+  blanksArray[letterIndex] = letterLower;
+  console.log(blanksArray);
+
+}
+*/
+
+let letterIndex = wordArray.indexOf(letterLower);
+blanksArray[letterIndex] = letterLower;
+//newBlanks = blanksArray.join
+console.log(blanksArray.join(" "));
+
+// Sample code from class
+/*
+let feedback = prompt.question("How's it going'?: ");
+console.log(feedback);
+
+if (feedback === "Good") {
+  console.log("I'm glad!");
+} else {
+  console.log("I'm so sorry to hear that.");
+}
+*/

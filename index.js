9650d5c590c70535ade9ebd1f0871cb93f118531
pmermod <93401090+pmermod@users.noreply.github.com>
2022-11-23 13:53:56 -0500

    Develop filling blanks array with multiple same letters in word

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import prompt from "readline-sync";
 //import wordBank from "./word-bank.js";
-const wordBank = ["win"]; // REMOVE BEFORE SUBMITTING AND UNCOMMENT LINE ABOVE
+const wordBank = ["fossil"]; // REMOVE BEFORE SUBMITTING AND UNCOMMENT LINE ABOVE
 
 console.log(
   "\nLookout! \n .... It's HaNGMaN!\n\nPress ctrl + c to exit.\n\n\n"
@@ -10,16 +10,16 @@ console.log(
   "Guess the letters that make up the secret word, one letter at a time. \nYou are allowed six wrong guesses before you are hung.\n"
 );
 
-let randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-let wordLowCase = randomWord.toLowerCase();
-let wordArray = wordLowCase.split("");
+let randomWord = wordBank[Math.floor(Math.random() * wordBank.length)]; //gets random word
+let wordLower = randomWord.toLowerCase();
+let wordArray = wordLower.split("");
 let blanksLength = wordArray.length;
 
 // test
 wordArray;
 
 // To print number of blanks equal to number of letters in word (before first guess).
-// First, the "blanks" (underscore character) are put into an array so that they can later be replaced by any correcty guessed letters.
+// First, the "blanks" (underscore character) are put into an array so that they can later be replaced by any correctly guessed letters.
 
 let blankString = "_";
 let blanksArray = blankString.repeat(blanksLength).split("");
@@ -29,26 +29,31 @@ blanksArray;
 
 // Then the array of blanks is printed to console as a string.
 
-let allBlanksString = blanksArray.join(" ");
-console.log(allBlanksString);
+console.log(blanksArray.join(" "));
 
 //UNCOMMENT LINE BELOW!! *********************************************
 //let input = prompt.question("Please guess a letter: ");
 
-let input = "n";
+let input = "s";
 
 //console.log(input);
 
 // Make letter lowercase
 let letter = input.toLowerCase();
-let containsLetter = wordArray.includes(letter);
+//let containsLetter = wordArray.includes(letter);
 
-// If letter is included in word, fill in corresp. blanks with letters
+/* 
+If letter is included in word, fill in corresp. blanks with letters
+
 if (containsLetter) {
   let letterIndex = wordArray.indexOf(letter);
   blanksArray[letterIndex] = letter;
   console.log(blanksArray.join(" "));
-} // WILL THIS WORK ON REPEATED ROUNDS. WILL IT PRINT THE BLANKS ARRAY WITH ONLY THE CURRENTLY GUESSED LETTER FILLED IN. WAIT MAYBE NOT. HOW CAN I TEST THIS? RUN NODE .
+} 
+
+
+///*********************************** USE THIS FOR ELSE CODE BELOW 
+
 else {
   let badGuessNum = 1;
   console.log(
@@ -57,6 +62,15 @@ else {
     } incorrect guesses left.`
   );
 }
+*/
+
+//If letter is included in word, fill in corresp. blanks with letters. Works with multiple same letters in word.
+
+wordArray.forEach((currentItem, index) => {
+  currentItem === letter ? (blanksArray[index] = letter) : null;
+});
+
+console.log(blanksArray.join(" "));
 
 // Sample code from class
 /*

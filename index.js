b196c7df0aa23dfcd1201fefcab874d4d3b3874d
pmermod <93401090+pmermod@users.noreply.github.com>
2022-11-29 20:12:40 -0500

    Restarted code. Set up while loop

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 import prompt from "readline-sync";
-//import wordBank from "./word-bank.js";
-const wordBank = ["fosSil"]; // REMOVE BEFORE SUBMITTING AND UNCOMMENT LINE ABOVE
-
-console.log("\nLookout! \n .... It's HaNGMaN!\n\nPress ctrl + c to exit.\n\n");
-
-console.log(
-  "Guess the secret word, one letter at a time. \nYou are allowed five incorrect guesses before you are hanged.\n"
-);
-
-const randomWord = wordBank[Math.floor(Math.random() * wordBank.length)]; //gets random word
-
-const wordArray = randomWord.toLowerCase().split("");
-let blanksLength = wordArray.length;
+import wordBank from "./word-bank.js";
 
 // Variables containing the stick figures to be printed based on number of incorrect guesses (badGuessNum)
 const fig1 = "O\n |";
@@ -23,95 +11,50 @@ const fig6 = "O\n\\|/\n |\n/ \\ \n\nSorry, You Lose";
 
 const printFigure = (badGuessNum) => {
   if (badGuessNum === 1) {
-    console.log("\n", fig1, "\n", blanksArray);
+    console.log("\n", fig1, "\n", blanksArr);
   } else if (badGuessNum === 2) {
-    console.log("\n", fig2, "\n", blanksArray);
+    console.log("\n", fig2, "\n", blanksArr);
   } else if (badGuessNum === 3) {
-    console.log("\n", fig3, "\n", blanksArray);
+    console.log("\n", fig3, "\n", blanksArr);
   } else if (badGuessNum === 4) {
-    console.log("\n", fig4, "\n", blanksArray);
+    console.log("\n", fig4, "\n", blanksArr);
   } else if (badGuessNum === 5) {
-    console.log("\n", fig5, "\n", blanksArray);
+    console.log("\n", fig5, "\n", blanksArr);
   } else {
-    console.log("\n", fig6, "\n", blanksArray);
+    console.log("\n", fig6, "\n", blanksArr);
   }
 };
 
-// test
-wordArray;
+const randomWord = wordBank[Math.floor(Math.random() * wordBank.length)]; //gets random word
+const wordArr = randomWord.toLowerCase().split("");
 
-// To print number of blanks equal to number of letters in word (below first prompt).
-// First, the "blanks" (underscore character) are put into an array so that they can later be replaced by correctly guessed letters.
+wordArr;
 
-let blankString = "_";
-let blanksArray = blankString.repeat(blanksLength).split("");
+const blankString = "_";
+let blanksArr = blankString.repeat(wordArr.length).split("");
 
-// test
-blanksArray;
+//blanksArr
 
-// Then the array of blanks is printed to console as a string.
+console.log("\nLookout! \n .... It's HaNGMaN!\n\nPress ctrl + c to exit.\n\n");
+console.log(
+  "Guess the secret word, one letter at a time. \nYou are allowed five incorrect guesses before you lose the game.\n"
+);
 
-console.log(blanksArray.join(" "));
+let badGuessNum = 0;
 
-//********************************************* UNCOMMENT LINE BELOW!!
-//let input = prompt.question("Please guess one letter: ");
+while (badGuessNum < 6 && blanksArr !== wordArr) {
+  let input = prompt.question("Please guess a letter: ");
 
-let input = "S"; // DELETE THIS LINE ***************************
-
-// Tests if input is not a letter and prints error mssg and re-prompts
-if (/[^a-zA-Z]/.test(input)) {
-  console.log("\n\nPlease try again, English letters only.");
-  //let input = prompt.question("Please guess one letter: ");
-}
-let letter = input.toLowerCase();
-
-//If letter is included in word, fill in corresp. blanks in blanksArr with letter(s). Works with letter appearing multiple times in word.
-
-wordArray.forEach((currentItem, index) => {
-  currentItem === letter ? (blanksArray[index] = letter) : printFigure(); //NEED badGuessNum COUNTER
-});
-
-// ^I need function here for the Else
-/*
-for (let i = 0; 0 < wordArray.length; i++) {
-  if (wordArray[i] === letter) {
-    blanksArray[i] = letter;
-    console.log("\n\n" + blanksArray.join(" "));
+  if (/[^a-zA-Z]/.test(input)) {
+    //if NOT an alpha character
+    console.log("\n\nPlease try again, letters only.");
+    let input = prompt.question("Please guess a letter: "); //WHAT IF NON-ALPHA ENTERED AGAIN?
   } else {
-    let badGuessNum = 1;
-  } // ************************************CONTINUE HERE
+    let letter = input.toLowerCase();
+  }
+
+  wordArr.forEach((currentItem, index) => {
+    currentItem === letter ? (blanksArr[index] = letter) : badGuessNum++;
+    printFigure();
+  });
 }
-*/
-// console.log("\n\n" + blanksArray.join(" "));
-
-// TRYING TO DEVELOP A FUNCTION FOR THE ELSE CONDITION ABOVE
-/*
-if (badGuessNum <= 6) {
-  console.log(
-    `\nThe letter ${letter} is not in the word.\nYou have ${
-      6 - badGuessNum
-    } incorrect guesses left.`
-  );
-}
-*/
-
-// Sample code from class
-/*
-let feedback = prompt.question("How's it going'?: ");
-console.log(feedback);
-
-if (feedback === "Good") {
-  console.log("I'm glad!");
-} else {
-  console.log("I'm so sorry to hear that.");
-}
-*/
-
-/* If letter is included in word, fill in corresp. blanks with letters
-
-if (containsLetter) {
-  let letterIndex = wordArray.indexOf(letter);
-  blanksArray[letterIndex] = letter;
-  console.log(blanksArray.join(" "));
-} 
-*/

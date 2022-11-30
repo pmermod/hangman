309a644c1070c88021e5931408047ba162d05d8b
pmermod <93401090+pmermod@users.noreply.github.com>
2022-11-29 22:38:49 -0500

    Correct while loop conditions. Test winner and loser. Seems to work.

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ const fig6 = "O\n\\|/\n |\n/ \\ \n\nSorry, You Lose";
 
 const printFigure = (badGuessNum) => {
   if (badGuessNum === 1) {
-    console.log("\n", fig1, "\n", blanksArr);
+    console.log("\n", fig1, "\n");
   } else if (badGuessNum === 2) {
-    console.log("\n", fig2, "\n", blanksArr);
+    console.log("\n", fig2, "\n");
   } else if (badGuessNum === 3) {
-    console.log("\n", fig3, "\n", blanksArr);
+    console.log("\n", fig3, "\n");
   } else if (badGuessNum === 4) {
-    console.log("\n", fig4, "\n", blanksArr);
+    console.log("\n", fig4, "\n");
   } else if (badGuessNum === 5) {
-    console.log("\n", fig5, "\n", blanksArr);
+    console.log("\n", fig5, "\n");
   } else {
-    console.log("\n", fig6, "\n", blanksArr);
+    console.log("\n", fig6, "\n");
   }
 };
 
@@ -36,15 +36,15 @@ let blanksArr = blankString.repeat(wordArr.length).split("");
 
 //blanksArr;
 
-console.log("\nLookout! \n .... It's HaNGMaN!\n\nPress ctrl + c to exit.\n\n");
+console.log("\nLookout! \n .... It's HaNGMaN!\n\nPress ctrl + c to exit.\n");
 console.log(
   "Guess the secret word, one letter at a time. \nYou are allowed five incorrect guesses before you lose the game.\n"
 );
 
 let badGuessNum = 0;
 
-while (badGuessNum < 6 && blanksArr !== wordArr) {
-  console.log(blanksArr.join(" "));
+while (badGuessNum < 6 && blanksArr.indexOf("_") !== -1) {
+  console.log("\n" + blanksArr.join(" "));
   let input = prompt.question("\nPlease guess a letter: ");
   let letter = input.toLowerCase();
   /*
@@ -59,20 +59,16 @@ while (badGuessNum < 6 && blanksArr !== wordArr) {
 
   wordArr.forEach((currentItem, index) => {
     currentItem === letter ? (blanksArr[index] = letter) : null;
-    //printFigure();
   });
-  //console.log("Incorrect Guess Number: " + badGuessNum);
 
-  /*
-  let containsLetter = wordArr.includes(letter);
-
-  if (containsLetter) {
-    let letterIndex = wordArr.indexOf(letter); //NEED A Check for multiple same letters ****** !!!!
-    blanksArr[letterIndex] = letter;
+  if (blanksArr.indexOf("_") === -1) {
     console.log(blanksArr.join(" "));
-  } else {
-    badGuessNum++;
+    console.log("\nW I N N E R !!!");
   }
-  */
-  //console.log("Incorrect Guess Number: " + badGuessNum);
+
+  if (wordArr.indexOf(letter) === -1) {
+    badGuessNum++;
+    console.log(`\nIncorrect Guess Number: ${badGuessNum}`);
+    printFigure(badGuessNum);
+  }
 }
